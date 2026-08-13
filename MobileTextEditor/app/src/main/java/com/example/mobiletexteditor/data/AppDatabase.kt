5254730ae4680eebda815a7c3394008a1d400c27
DinaraWickrama <thiyuminidinara@gmail.com>
2026-08-13 20:54:39 +0530

    small fixes

--- MobileTextEditor/app/src/main/java/com/example/mobiletexteditor/data/AppDatabase.kt
+++ MobileTextEditor/app/src/main/java/com/example/mobiletexteditor/data/AppDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities = [FileEntity::class, VersionEntity::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
@@ -23,7 +23,9 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "mobile_text_editor.db"
-                ).build().also { INSTANCE = it }
+                )
+                    .fallbackToDestructiveMigration()
+                    .build().also { INSTANCE = it }
             }
     }
 }
